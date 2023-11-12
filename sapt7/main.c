ef9f5dfe06bc3beac715b43dbb4802a376a6251f
Norbi76 <kondora35@gmail.com>
2023-11-12 17:11:02 +0200

    implemented first two requirements

--- sapt7/main.c
+++ sapt7/main.c
@@ -5,7 +5,11 @@
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<time.h>
 
+# define BUFFER_SIZE 200 
 # define FIELD_SIZE 100
 
 typedef struct {
@@ -14,9 +18,100 @@ typedef struct {
     time_t timpUltimaModificare;
 }myStat;
 
+void getData(int in, myStat *stat, char *numeFisier) {
+    struct stat fileStat;
+    int buffer;
+
+    strcpy(stat->numeFisier, numeFisier);
+
+    lseek(in, 18, SEEK_SET);
+    read(in, &buffer, 4);
+    stat->dimensiune = buffer;
+
+    if (strstr(numeFisier, ".bmp")) {
+        read(in, &buffer, 4);
+        stat->lungime = buffer;
+
+        read(in, &buffer, 4);
+        stat->inaltime = buffer;
+    }
+    else {
+        stat->lungime = -1;
+        stat->inaltime = -1;
+    }
+
+    fstat(in, &fileStat);
+    stat->nrLegaturi = fileStat.st_nlink;
+    stat->userId = fileStat.st_uid;
+    stat->timpUltimaModificare = fileStat.st_mtime;
+
+    (S_ISDIR(fileStat.st_mode)) ? strcpy(stat->drepturiUser, "d") : strcpy(stat->drepturiUser, "-");
+    (fileStat.st_mode & S_IRUSR) ? strcat(stat->drepturiUser, "r") : strcat(stat->drepturiUser, "-");
+    (fileStat.st_mode & S_IWUSR) ? strcat(stat->drepturiUser, "w") : strcat(stat->drepturiUser, "-");
+    (fileStat.st_mode & S_IXUSR) ? strcat(stat->drepturiUser, "x") : strcat(stat->drepturiUser, "-");
+
+    (fileStat.st_mode & S_IRGRP) ? strcpy(stat->drepturiGrup, "r") : strcpy(stat->drepturiGrup, "-");
+    (fileStat.st_mode & S_IRGRP) ? strcat(stat->drepturiGrup, "w") : strcat(stat->drepturiGrup, "-");
+    (fileStat.st_mode & S_IXGRP) ? strcat(stat->drepturiGrup, "x") : strcat(stat->drepturiGrup, "-");
+
+    (fileStat.st_mode & S_IROTH) ? strcpy(stat->drepturiAltii, "r") : strcpy(stat->drepturiAltii, "-");
+    (fileStat.st_mode & S_IWOTH) ? strcat(stat->drepturiAltii, "w") : strcat(stat->drepturiAltii, "-");
+    (fileStat.st_mode & S_IXOTH) ? strcat(stat->drepturiAltii, "x") : strcat(stat->drepturiAltii, "-");
+
+}
+
+void fprintStat(myStat stat, int out) {
+    char buffer[BUFFER_SIZE];
+
+    sprintf(buffer, "Nume fisier: %s\n", stat.numeFisier);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+
+    sprintf(buffer, "Dimensiune fisier: %d\n", stat.dimensiune);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+
+    if (stat.inaltime != -1 || stat.lungime != -1) {
+        sprintf(buffer, "Inaltime imagine: %d\n", stat.inaltime);
+        write(out, buffer, strlen(buffer));
+        strcpy(buffer, "");
+
+        sprintf(buffer, "Lungime imagine: %d\n", stat.lungime);
+        write(out, buffer, strlen(buffer));
+        strcpy(buffer, "");
+    }
+
+    sprintf(buffer, "Identificatorul utilizatorului: %d\n", stat.userId);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+
+    sprintf(buffer, "Contor de legaturi: %d\n", stat.nrLegaturi);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+
+    sprintf(buffer, "Timpul ultimei modificari: %s", ctime(&stat.timpUltimaModificare));
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+
+    sprintf(buffer, "Drepturi de acces user: %s\n", stat.drepturiUser);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+
+    sprintf(buffer, "Drepturi de acces grup: %s\n", stat.drepturiGrup);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+
+    sprintf(buffer, "Drepturi de acces altii: %s\n\n", stat.drepturiAltii);
+    write(out, buffer, strlen(buffer));
+    strcpy(buffer, "");
+
+    
+}
+
 int main(int argc, char **argv) {
     struct stat fileStat;
     struct dirent *dirent;
+    myStat statistic;
     DIR *dir;
 
     if (argc != 2) {
@@ -31,27 +126,30 @@ int main(int argc, char **argv) {
     }
 
     while ((dirent = readdir(dir)) != NULL) {
-        // printf("%s\n", dirent->d_name);
         char relativePath[50];
         strcpy(relativePath, argv[1]);
         strcat(relativePath, "/");
         strcat(relativePath, dirent->d_name);
-        // printf("%s\n", relativePath);
         
         if ((stat(relativePath, &fileStat) == -1)) {
             printf("Something went wrong!\n");
             exit(1);
         }
-
-        // printf("%d\n", S_ISREG(fileStat.st_mode));
         
         if (S_ISREG(fileStat.st_mode)) {
-            if (strstr(relativePath, ".bmp")) {
-                printf("%s\n", relativePath);
+            int in = open(relativePath, O_RDONLY);
+            int out = open("statistica.txt", O_WRONLY | O_APPEND);
+
+            if (in == -1 || out == -1) {
+                printf("Something went wrong!\n");
+                exit(1);
             }
-            else {
-                printf("%s\n", relativePath);
-            }
+
+            getData(in, &statistic, dirent->d_name);
+            fprintStat(statistic, out);
+
+            close(in);
+            close(out);
         }
 
     }
